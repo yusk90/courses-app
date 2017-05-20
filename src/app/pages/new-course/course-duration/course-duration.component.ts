b18a5034b4c4a7381ab2f925628c4fce93ef1129
Yurii Skorniakov <yurii.skorniakov@gmail.com>
2017-05-21 00:26:03 +0300

    Implement ControlValueAccessor for course duration component

--- src/app/pages/new-course/course-duration/course-duration.component.ts
+++ src/app/pages/new-course/course-duration/course-duration.component.ts
@@ -1,21 +1,55 @@
 import {
   Component,
-  Input,
-  Output,
-  EventEmitter
+  Input
 } from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR
+} from '@angular/forms';
 
 @Component({
   selector: 'course-duration',
   styleUrls: [ './course-duration.scss' ],
-  templateUrl: './course-duration.html'
+  templateUrl: './course-duration.html',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: CourseDurationComponent,
+    multi: true
+  }]
 })
 
-export class CourseDurationComponent {
+export class CourseDurationComponent implements ControlValueAccessor {
   @Input()
   public inputId: string;
-  @Input()
-  public duration: number;
-  @Output()
-  public durationChange: EventEmitter<any> = new EventEmitter();
+
+  private innerValue: any = '';
+  private propagateChange = (_: any) => {};
+  private onTouchedCallback = () => {};
+
+  get duration(): any {
+    return this.innerValue;
+  };
+
+  set duration(value: any) {
+    if (value !== this.innerValue) {
+      this.innerValue = (value !== null) ? value : '';
+      this.propagateChange(this.innerValue);
+    }
+  }
+
+  public writeValue(value: any) {
+    this.duration = value;
+  }
+
+  public registerOnChange(fn) {
+    this.propagateChange = fn;
+  }
+
+  public registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
+
+  public onBlur() {
+    this.onTouchedCallback();
+  }
 }

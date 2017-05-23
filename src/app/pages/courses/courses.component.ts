4f6dbb40538afd1a915ce0be6db9225c781f7089
yusk90 <yurii.skorniakov@gmail.com>
2017-05-23 19:34:32 +0300

    Implement backend search

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -17,29 +17,20 @@ import {
 })
 
 export class CoursesComponent implements OnInit {
-  public courses: Course[] = [];
+  public coursesState;
   public courseIdForDelete: number;
   public orderDirections: string[] = [ 'asc', 'desc' ];
-  private cachedCourses: Course[] = [];
 
   constructor(
-    private coursesService: CoursesService,
+    public coursesService: CoursesService,
     private confirmationModalService: ConfirmationModalService,
     private loaderBlockService: LoaderBlockService,
     private datePipe: DatePipe
   ) {}
 
   public ngOnInit() {
-    this.coursesService.getCourses().then((courses) => {
-      this.courses = this.prepareCourses(courses);
-      this.cacheCourses();
-    });
-  }
-
-  public filter(query: string): void {
-    const properties = [ 'title', 'displayDate' ];
-
-    this.courses = query ? this.filterBy(properties, query) : this.cachedCourses;
+    this.coursesService.getCourses();
+    this.coursesState = this.coursesService.getState();
   }
 
   public deleteCourse(id: number): void {
@@ -47,7 +38,8 @@ export class CoursesComponent implements OnInit {
     this.coursesService.deleteCourse(id)
       .then(() => {
         this.loaderBlockService.hide();
-        this.courses.splice(this.courses.findIndex((course) => course.id === id), 1);
+        this.coursesState.courses
+          .splice(this.coursesState.courses.findIndex((course) => course.id === id), 1);
       });
   }
 
@@ -58,26 +50,5 @@ export class CoursesComponent implements OnInit {
       cancelButtonText: 'No',
       onSubmit: this.deleteCourse.bind(this, courseId)
     });
-  }
-
-  private prepareCourses(courses) {
-    return courses.map((course: Course) => {
-      const preparedCourse = Object.assign({}, course);
-
-      preparedCourse.displayDate = this.datePipe.transform(course.date, 'dd-MM-yyyy');
-      preparedCourse.dateInMs = new Date(course.date).getTime();
-      preparedCourse.formattedAuthors = course.authors.join(', ');
-      return preparedCourse;
-    });
-  }
-
-  private filterBy(properties: string[], query: string): Course[] {
-    return this.courses.filter((course: Course) => {
-      return properties.some((property) => course[property].search(query) > -1);
-    });
-  }
-
-  private cacheCourses(): void {
-    this.cachedCourses = this.courses;
   }
 }
--- src/app/shared/courses.service.ts
+++ src/app/shared/courses.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { DatePipe } from '@angular/common';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -7,18 +8,27 @@ import { AuthorizationService } from './authorization.service';
 import { Course } from './course-interface';
 import { API_URL } from './constants';
 
+const state = {
+  courses: []
+};
+
 @Injectable()
 export class CoursesService {
   constructor(
     private http: Http,
-    private authorizationService: AuthorizationService
+    private authorizationService: AuthorizationService,
+    private datePipe: DatePipe
   ) {}
 
-  public getCourses(): Promise<Course[]> {
+  public getState() {
+    return state;
+  }
+
+  public getCourses(query = '') {
     return this.http
-      .get(`${ API_URL }/courses?access_token=${ this.authorizationService.getToken() }`)
+      .get(`${ API_URL }/courses?${ query }access_token=${ this.authorizationService.getToken() }`)
       .toPromise()
-      .then((response) => response.json() as Course[]);
+      .then((response) => state.courses = this.prepareCourses(response.json()));
   }
 
   public getCourseById(id: number): Promise<Course> {
@@ -44,5 +54,38 @@ export class CoursesService {
     return this.http
       .patch(`${ API_URL }/courses/${ course.id }?access_token=${ this.authorizationService.getToken() }`, course)
       .toPromise();
+  }
+
+  public filter(query: string): void {
+    this.getCourses(query && this.generateFilterQuery(query));
+  }
+
+  private generateFilterQuery(query: string) {
+    const properties = [ 'title', 'description' ];
+
+    const searchArray = properties.map((field) => {
+      return {
+        [field]: {
+          like: query
+        }
+      };
+    });
+
+    return 'filter=' + JSON.stringify({
+      where: {
+        or: searchArray
+      }
+    }) + '&';
+  }
+
+  private prepareCourses(courses) {
+    return courses.map((course: Course) => {
+      const preparedCourse = Object.assign({}, course);
+
+      preparedCourse.displayDate = this.datePipe.transform(course.date, 'dd-MM-yyyy');
+      preparedCourse.dateInMs = new Date(course.date).getTime();
+      preparedCourse.formattedAuthors = course.authors.join(', ');
+      return preparedCourse;
+    });
   }
 }
